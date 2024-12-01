e38b410f7881b893d295725c1c518adbfe59b730
artbek <bart@artbek.co.uk>
2024-12-01 22:19:17 +0000

    [Day 01]: part 2.

--- day01/Day01.hs
+++ day01/Day01.hs
@@ -1,19 +1,47 @@
 module Day01 where
 
-import Data.List
+import Data.List (sort)
 
 main = do
     answer_1_test <- sumOfDistances <$> readFile "test_input.txt"
-    answer_1 <- sumOfDistances <$> readFile "input.txt"
+    answer_1_live <- sumOfDistances <$> readFile "input.txt"
     putStrLn $ "Part 1: " ++ (show answer_1_test) ++ " (test)"
-    putStrLn $ "Part 1: " ++ (show answer_1) ++ " (live)"
+    putStrLn $ "Part 1: " ++ (show answer_1_live) ++ " (live)"
+
+    answer_2_test <- part2 <$> readFile "test_input.txt"
+    answer_2_live <- part2 <$> readFile "input.txt"
+    putStrLn $ "Part 2: " ++ (show answer_2_test) ++ " (test)"
+    putStrLn $ "Part 2: " ++ (show answer_2_live) ++ " (live)"
+
+
+-- Part 1
 
 sumOfDistances :: String -> Int
 sumOfDistances ss = sum $ map (\ (x,y) -> abs (x - y)) (sortedLists ss)
 
 sortedLists :: String -> [(Int, Int)]
-sortedLists ss = zip (sort leftList) (sort rightList)
-    where
-        leftList  = map (read . head . words) . lines $ ss
-        rightList = map (read . last . words) . lines $ ss
+sortedLists ss = zip (sort (leftList ss)) (sort (rightList ss))
 
+
+-- Part 2
+
+countElement :: Int -> [Int] -> Int
+countElement el []     = 0
+countElement el (x:xs) = (if (el == x) then 1 else 0) + countElement el xs
+
+similarityScore :: [Int] -> [Int] -> Int
+similarityScore []     ys = 0
+similarityScore (x:xs) ys = x * (countElement x ys) + similarityScore xs ys
+
+
+part2 :: String -> Int
+part2 ss = similarityScore (leftList ss) (rightList ss)
+
+
+-- Helpers
+
+leftList :: String -> [Int]
+leftList = map (read . head . words) . lines
+
+rightList :: String -> [Int]
+rightList = map (read . last . words) . lines
