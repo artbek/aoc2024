759ca8083884e5f8709f6625d85062ef68fe109c
artbek <bart@artbek.co.uk>
2024-12-03 22:49:33 +0000

    [Day 03]: part 2.

--- day03/Day03.hs
+++ day03/Day03.hs
@@ -10,17 +10,17 @@ main = do
     answer_1_live <- part1 <$> readFile "input.txt"
     putStrLn $ "Part 1: " ++ (show answer_1_live) ++ " (live)"
 
-    --answer_2_test <- part2 <$> readFile "test_input.txt"
-    --putStrLn $ "Part 2: " ++ (show answer_2_test) ++ " (test)"
+    answer_2_test <- part2 <$> readFile "test_input2.txt"
+    putStrLn $ "Part 2: " ++ (show answer_2_test) ++ " (test)"
 
-    --answer_2_live <- part2 <$> readFile "input.txt"
-    --putStrLn $ "Part 2: " ++ (show answer_2_live) ++ " (live)"
+    answer_2_live <- part2 <$> readFile "input.txt"
+    putStrLn $ "Part 2: " ++ (show answer_2_live) ++ " (live)"
 
 
 -- Part 1
 
 part1 :: String -> Int
-part1 ss = sum $ map mul $ pairsOfNumbers (candidates ss)
+part1 = sum . map mul . pairsOfNumbers . candidates
 
 candidates :: String -> [String]
 candidates = filter (isPrefixOf "mul(") . map (take 12) . tails
@@ -49,7 +49,17 @@ argIsValid aa = all isDigit aa && elem (length aa) [1..3]
 -- Part 2
 
 part2 :: String -> Int
-part2 ss = 0
+part2 = sum . map mul . pairsOfNumbers . candidates . parse Enabled
+
+data Mode = Enabled | Disabled deriving (Eq)
+
+parse :: Mode -> String -> String
+parse mode [] = ""
+parse mode (x:xs)
+    | isPrefixOf "don't()" (x:xs) = x : (parse Disabled xs)
+    | isPrefixOf "do()" (x:xs)    = x : (parse Enabled xs)
+    | mode == Enabled             = x : (parse mode xs)
+    | mode == Disabled            = '_' : (parse mode xs)
 
 
 -- Helpers
