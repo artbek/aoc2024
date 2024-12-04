bba616452a541831696c6c9059134d6529e28c1f
artbek <bart@artbek.co.uk>
2024-12-04 23:17:48 +0000

    [Day 04]: part 2.

--- day04/Day04.hs
+++ day04/Day04.hs
@@ -1,6 +1,6 @@
 module Day04 where
 
-import Data.List (inits, tails, isPrefixOf, transpose)
+import Data.List (inits, tails, isPrefixOf, sort, transpose)
 
 main = do
     answer_1_test <- part1 <$> readFile "test_input.txt"
@@ -9,11 +9,11 @@ main = do
     answer_1_live <- part1 <$> readFile "input.txt"
     putStrLn $ "Part 1: " ++ (show answer_1_live) ++ " (live)"
 
-    -- answer_2_test <- part2 <$> readFile "test_input2.txt"
-    -- putStrLn $ "Part 2: " ++ (show answer_2_test) ++ " (test)"
+    answer_2_test <- part2 <$> readFile "test_input.txt"
+    putStrLn $ "Part 2: " ++ (show answer_2_test) ++ " (test)"
 
-    -- answer_2_live <- part2 <$> readFile "input.txt"
-    -- putStrLn $ "Part 2: " ++ (show answer_2_live) ++ " (live)"
+    answer_2_live <- part2 <$> readFile "input.txt"
+    putStrLn $ "Part 2: " ++ (show answer_2_live) ++ " (live)"
 
 
 -- Part 1
@@ -63,5 +63,21 @@ rot45 ss = transpose $ zipWith (++) padding (rot45part ss)
 -- Part 2
 
 part2 :: String -> Int
-part2 ss = 0
+part2 ss = length $ filter (==True) (scanInput 0 0 (lines ss))
 
+scanInput :: Int -> Int -> [[Char]] -> [Bool]
+scanInput r c ss
+    | c >= (length ss - 2) = []
+    | otherwise = hasX (c1:c3:c5:c7:c9:"") : scanInput nextR nextC ss
+    where
+        nextR = mod (r+1) (length ss - 2)
+        nextC = if (nextR == 0) then (c+1) else c
+        c1 = ss!!r!!c
+        c3 = ss!!r!!(c+2)
+        c5 = ss!!(r+1)!!(c+1)
+        c7 = ss!!(r+2)!!c
+        c9 = ss!!(r+2)!!(c+2)
+
+hasX :: [Char] -> Bool
+hasX (a:b:'A':c:d:_) = elem [a,b,c,d] ["MMSS", "SSMM", "MSMS", "SMSM"]
+hasX _ = False
