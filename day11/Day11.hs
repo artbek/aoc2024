57b148da53fd37ad2cbf7c72adbb98de1290368a
artbek <bart@artbek.co.uk>
2024-12-14 20:44:59 +0000

    [Day 11]: part 2 (memoize).

--- day11/Day11.hs
+++ day11/Day11.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Data.Function.Memoize
+
 
 main = do
     let answer_1_test = part1 "125 17"
@@ -8,17 +10,20 @@ main = do
     answer_1_live <- part1 <$> readFile "input.txt"
     putStrLn $ "Part 1: " ++ (show answer_1_live) ++ " (live)"
 
-    -- answer_2_test <- part2 <$> readFile "test_input5.txt"
+    -- let answer_2_test = part2 "125 17"
     -- putStrLn $ "Part 2: " ++ (show answer_2_test) ++ " (test)"
 
-    -- answer_2_live <- part2 <$> readFile "input.txt"
-    -- putStrLn $ "Part 2: " ++ (show answer_2_live) ++ " (live)"
+    answer_2_live <- part2 <$> readFile "input.txt"
+    putStrLn $ "Part 2: " ++ (show answer_2_live) ++ " (live)"
 
+
+maxBlinksP1 = 25 :: Int
+maxBlinksP2 = 75 :: Int
 
 -- Part 1 --
 
 part1 :: String -> Int
-part1 = length . last . take (25 + 1) . iterate blink . words
+part1 = length . last . take (maxBlinksP1 + 1) . iterate blink . words
 
 {-
     0) 125 17
@@ -49,4 +54,23 @@ dz ss = dropWhile (=='0') (init ss) ++ [last ss]
 -- Part 2 --
 
 part2 :: String -> Int
-part2 = length
+part2 = sum . map (blink2 (maxBlinksP2 - 1)) . map read . words
+
+blink2Memo :: Int -> Int -> Int
+blink2Memo = memoize2 blink2
+
+blink2 :: Int -> Int -> Int
+blink2 0 stone = length $ processStone' stone
+blink2 counter stone = sum $ map (blink2Memo (counter - 1)) newStones
+    where
+        newStones = processStone' stone
+
+processStone' :: Int -> [Int]
+processStone' stone
+    | stone == 0 = [1]
+    | (stoneLen) `mod` 2 == 0 = map read [take halfStone ss, drop halfStone ss]
+    | otherwise = [stone * 2024]
+    where
+        ss = show stone
+        stoneLen = length ss
+        halfStone = stoneLen `div` 2
