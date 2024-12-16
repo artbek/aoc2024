f1fdee7fd41c849bf3d4c7150fd42a24417e7287
artbek <bart@artbek.co.uk>
2024-12-16 12:01:34 +0000

    [Day 13]: part 2.

--- day13/Day13.hs
+++ day13/Day13.hs
@@ -8,11 +8,14 @@ main = do
     answer_1_test <- part1 <$> readFile "test_input.txt"
     putStrLn $ "(test) Part 1 (480): " ++ (show answer_1_test)
 
-    answer_1_2_test <- part1 <$> readFile "test_input_2.txt"
-    putStrLn $ "(test) Part 1.2 (168): " ++ (show answer_1_2_test)
-
     answer_1_live <- part1 <$> readFile "input.txt"
     putStrLn $ "(live) Part 1: " ++ (show answer_1_live)
+
+    -- answer_2_test <- part2 <$> readFile "test_input_1.txt"
+    -- putStrLn $ "(test) Part 2 (???): " ++ (show answer_2_test)
+
+    answer_2_live <- part2 <$> readFile "input.txt"
+    putStrLn $ "(live) Part 2: " ++ (show answer_2_live)
 
 
 -- Part 1 --
@@ -33,19 +36,29 @@ strToInput (line_a:line_b:line_p:_:ss) = [(x1,y1,p1,x2,y2,p2)] ++ strToInput ss
         p1 = read $ init $ tail $ dropWhile (/='=') (prizeData!!1)
         p2 = read $ tail $ dropWhile (/='=') (prizeData!!2)
 
+
 part1 :: String -> Int
-part1 = sum . map findSolution . strToInput . lines
+part1 = sum . map (findSolution 0) . strToInput . lines
+
+findSolution :: Int -> (Int,Int,Int,Int,Int,Int) -> Int
+findSolution offset (x1,y1,p1,x2,y2,p2)
+    | hasFoundSolution = calculateCost (sa,sb)
+    | otherwise        = 0
+    where
+        p1' = p1 + offset
+        p2' = p2 + offset
+        sa  = (p1' - x2*sb) `div` x1
+        sb  = (p2'*x1 - y1*p1') `div` (y2*x1 - y1*x2)
+
+        hasFoundSolution = testX sa sb && testY sa sb
+        testX a b        = (a*x1) + (b*x2) == p1'
+        testY a b        = (a*y1) + (b*y2) == p2'
 
 calculateCost :: (Int,Int) -> Int
 calculateCost (a,b) = a * 3 + b
 
-findSolution :: (Int,Int,Int,Int,Int,Int) -> Int
-findSolution (x1,y1,p1,x2,y2,p2)
-    | hasFoundSolution = cheapestSolution
-    | otherwise     = 0
-    where
-        solutions = [ (a,b) | a <- [0..100], b <- [0..100], testX a b && testY a b ]
-        testX a b = (a*x1)+(b*x2) == p1
-        testY a b = (a*y1)+(b*y2) == p2
-        hasFoundSolution = length solutions > 0
-        cheapestSolution = head $ sort $ map calculateCost solutions
+
+-- Part 2 --
+
+part2 :: String -> Int
+part2 = sum . map (findSolution 10000000000000) . strToInput . lines
