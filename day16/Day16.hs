2ef8087162033a61cdde3fc7e69a5bb0eaf61803
artbek <bart@artbek.co.uk>
2024-12-21 12:37:41 +0000

    [Day 16]: part 1 (bfs).

--- day16/Day16.hs
+++ day16/Day16.hs
@@ -6,11 +6,11 @@ import Data.List (groupBy, sortBy)
 
 
 main = do
-    -- answer_1_0_test <- part1 <$> readFile "sample_1.txt"
-    -- putStrLn $ "(test) Part 1.0a (1006): " ++ (show answer_1_0_test)
+    answer_1_0_test <- part1 <$> readFile "sample_1.txt"
+    putStrLn $ "(test) Part 1.0a (1006): " ++ (show answer_1_0_test)
 
-    -- answer_1_0_test <- part1 <$> readFile "sample_2.txt"
-    -- putStrLn $ "(test) Part 1.0b (1006): " ++ (show answer_1_0_test)
+    answer_1_0_test <- part1 <$> readFile "sample_2.txt"
+    putStrLn $ "(test) Part 1.0b (1006): " ++ (show answer_1_0_test)
 
     answer_1_0_test <- part1 <$> readFile "sample_3.txt"
     putStrLn $ "(test) Part 1.0c (3010): " ++ (show answer_1_0_test)
@@ -27,8 +27,15 @@ main = do
 
 -- Data --
 
+data Sq = Sq { val :: Char
+             , n :: Int
+             , e :: Int
+             , s :: Int
+             , w :: Int
+             } deriving Show
+
 type Pos = (Int, Int)
-type Maze = Array Pos Char
+type Maze = Array Pos Sq
 type Tile = (Pos, Cost, Dir)
 type Cost = Int
 type Dir = Char
@@ -37,38 +44,32 @@ type Dir = Char
 -- Part 1 --
 
 strToMaze :: String -> Maze
-strToMaze ss = listArray ((0,0), (maxRow, maxCol)) $ concat $ lines ss
+strToMaze ss = listArray ((0,0), (maxRow, maxCol)) $ [ Sq v 0 0 0 0 | v <- vs ]
     where
-        maxRow = mazeSize - 1
+        vs = concat $ lines ss
+        maxRow = length (lines ss) - 1
         maxCol = length (head (lines ss)) - 1
-        mazeSize = length $ lines ss
-
-
--- 89344 is too high
 
 part1 :: String -> Int
 part1 ss = bfs maze [startingTile] (maxBound::Int)
     where
         maze         = strToMaze ss
-        startingPos  = head [ p | p <- range $ bounds maze, maze!p == 'S' ]
+        startingPos  = head [ p | p <- range $ bounds maze, val (maze!p) == 'S' ]
         startingTile = (startingPos, 0, '>')
 
 bfs :: Maze -> [Tile] -> Cost -> Cost
 bfs maze    [] minCost = minCost
-bfs maze tiles minCost = -- trace (mazeToStr maze' ++ "\n")
-                         bfs maze' (getUnique neighbours) minCost'
+bfs maze tiles minCost = bfs maze' (neighbours) minCost'
     where
-        maze'      = maze // [ (pos, 'O') | (pos,_,_) <- tiles ]
+        maze'      = maze // [ (p, visitedSq (maze!p) d c) | (p,c,d) <- tiles ]
         neighbours = foldl (\b a -> b ++ fst (findNbrs maze' a)) [] tiles
         minCost'   = foldl (\b a -> min b (snd (findNbrs maze' a))) minCost tiles
 
-getUnique :: [Tile] -> [Tile]
-getUnique tiles = deduped
-    where
-        sorted1 = sortBy (\(p1,_,_) (p2,_,_) -> compare p1 p2 ) tiles
-        grouped = groupBy (\(p1,_,_) (p2,_,_) -> p1 == p2) sorted1
-        sorted2 = map (sortBy (\(_,c1,_) (_,c2,_) -> compare c1 c2 )) grouped
-        deduped = map (head) sorted2
+visitedSq :: Sq -> Dir -> Cost -> Sq
+visitedSq s '^' cost = s { n = cost }
+visitedSq s 'v' cost = s { s = cost }
+visitedSq s '>' cost = s { e = cost }
+visitedSq s '<' cost = s { n = cost }
 
 findNbrs :: Maze -> Tile -> ([Tile], Int)
 findNbrs maze t@((r,c), cost, dir) = (legalTiles, minCost)
@@ -77,9 +78,25 @@ findNbrs maze t@((r,c), cost, dir) = (legalTiles, minCost)
         tS = ((r+1, c), newCost cost dir 'v', 'v')
         tE = ((r, c+1), newCost cost dir '>', '>')
         tW = ((r, c-1), newCost cost dir '<', '<')
-        legalTiles = filter (\(pos,_,_) -> maze!pos == '.') [tN, tS, tE, tW]
-        exitTiles  = filter (\(pos,_,_) -> maze!pos == 'E') [tN, tS, tE, tW]
+        legalTiles = filter (isLegal maze dir) [tN, tS, tE, tW]
+        exitTiles  = filter (\(pos,_,_) -> val (maze!pos) == 'E') [tN, tS, tE, tW]
         minCost    = foldl (\b (_,cost,_) -> min b cost) (maxBound::Int) exitTiles
+
+isLegal :: Maze -> Dir -> Tile -> Bool
+isLegal maze prevDir (pos, cost, dir)
+    | val sq == '#' = False
+    | cost > 90000  = False
+    | dir == '^' && n sq > 0 && n sq < cost = False
+    | dir == 'v' && s sq > 0 && s sq < cost = False
+    | dir == '>' && e sq > 0 && e sq < cost = False
+    | dir == '<' && w sq > 0 && w sq < cost = False
+    | prevDir == '^' && dir == 'v' = False
+    | prevDir == 'v' && dir == '^' = False
+    | prevDir == '>' && dir == '<' = False
+    | prevDir == '<' && dir == '>' = False
+    | otherwise = True
+    where
+        sq = maze!pos
 
 newCost :: Int -> Dir -> Dir -> Int
 newCost baseCost curDir newDir
@@ -90,7 +107,7 @@ newCost baseCost curDir newDir
 -- Helpers --
 
 mazeToStr :: Maze -> String
-mazeToStr maze = unlines $ splitEvery mazeWidth $ elems maze
+mazeToStr maze = unlines $ splitEvery mazeWidth $ map val $ elems maze
     where
         mazeWidth = (snd $ snd $ bounds maze) + 1
 
